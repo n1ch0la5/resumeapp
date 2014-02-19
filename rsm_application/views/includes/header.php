11df9c86f4c582c5e6b2727a321da1f6fb77b81a
zrichard <z.c.richard11@gmail.com>
2014-02-19 16:26:32 -0500

    moved previous code to head

--- rsm_application/views/includes/header.php
+++ rsm_application/views/includes/header.php
@@ -21,7 +21,6 @@
 		  <script src="/javascripts/respond.js" type="text/javascript"></script>
 		<![endif]-->
 
-	</head>
 	<?php 
 	$page_slug = end(explode ("/", $_SERVER["REQUEST_URI"]));
 	if (strlen($page_slug) > 0) {
@@ -31,6 +30,8 @@
 		$page_class = 'home';
 	}
 	?>
+
+	</head>
 	<body class="<?php echo $page_class; ?>">
 		<div class="wrapper">
 			<header>
