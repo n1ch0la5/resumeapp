fbbabe254691d43d47e96394063e3ddd9ca34aaa
Nick Johnson <nicholas.johnson78@gmail.com>
2014-02-20 11:38:33 -0500

    cleanup

--- rsm_application/views/home.php
+++ rsm_application/views/home.php
@@ -1,58 +1,57 @@
-
-				<main class="alpha" role="main">
-					<div class="feature-video">
-						<div class="content">
-							<h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h2>
-							<p>Pellenteque velit augue, elementum et interdum vel, molestie et ante.</p>
-							<a class="watch" href="#"><i class="fa fa-play-circle-o"></i> Watch Our Video</a>
-					    </div>
-					</div>
-					<div class="cta-bar is-sticky">
-						<p>Get started on your professional resume today.</p>
-						<a class="green" href="">Create a Resume</a>
-						<a class="white icon-arrow-right" href="">Sign Up</a>
-					</div>
-					<div class="trust">
-					</div>
-					<div class="primary three-col">
-						<h2>Build your professional resume in just a few minutes</h2>
-						<div class="col col1">
-							<div class="image"><img src="/images/file.png" alt="What is eRezzy?" /></div>
-							<h3>What is eRezzy?</h3>
-							<p>An online tool that helps you creat your resume quickly and professionally without the hassle and struggle of figuring out how to format everything.</p>
-							<p>With a good resume, you have the passport to a better job.</p>
-						</div>
-						<div class="col col2">
-							<div class="image"><img src="/images/macbook.png" alt="How Does it Work?" /></div>
-							<h3>How Does it Work?</h3>
-							<p>Enter your occupation (and experience), it will suggest the best template. Select key phrases where resume writing experience is not required.</p>
-							<p>Easily save and export to PDF, or publish online</p>
-						</div>
-						<div class="col col3">
-							<div class="image"><img src="/images/browser.png" alt="Key Phrases" /></div>
-							<h3>Key Phrases</h3>
-							<p>Search and customize thousands of professional phrases. You pick from samples written by experts. No more writers block.</p>
-							<p>We include context-sensitive resume writing tips</p>
-						</div>
-					</div>
-					<div class="secondary two-col">
-						<div class="col col1">
-							<h3>Heading</h3>
-							<ul class="arrow">
-								<li>List item</li>
-								<li>List item</li>
-								<li>List item</li>
-								<li>List item</li>
-							</ul>
-						</div>
-						<div class="col col2">
-							<h3>Heading</h3>
-							<ul class="arrow">
-								<li>List item</li>
-								<li>List item</li>
-								<li>List item</li>
-								<li>List item</li>
-							</ul>
-						</div>
-					</div>
-				</main>
+<main class="alpha" role="main">
+	<div class="feature-video">
+		<div class="content">
+			<h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h2>
+			<p>Pellenteque velit augue, elementum et interdum vel, molestie et ante.</p>
+			<a class="watch" href="#"><i class="fa fa-play-circle-o"></i> Watch Our Video</a>
+	    </div>
+	</div>
+	<div class="cta-bar is-sticky">
+		<p>Get started on your professional resume today.</p>
+		<a class="green" href="">Create a Resume</a>
+		<a class="white icon-arrow-right" href="">Sign Up</a>
+	</div>
+	<div class="trust">
+	</div>
+	<div class="primary three-col">
+		<h2>Build your professional resume in just a few minutes</h2>
+		<div class="col col1">
+			<div class="image"><img src="/images/file.png" alt="What is eRezzy?" /></div>
+			<h3>What is eRezzy?</h3>
+			<p>An online tool that helps you creat your resume quickly and professionally without the hassle and struggle of figuring out how to format everything.</p>
+			<p>With a good resume, you have the passport to a better job.</p>
+		</div>
+		<div class="col col2">
+			<div class="image"><img src="/images/macbook.png" alt="How Does it Work?" /></div>
+			<h3>How Does it Work?</h3>
+			<p>Enter your occupation (and experience), it will suggest the best template. Select key phrases where resume writing experience is not required.</p>
+			<p>Easily save and export to PDF, or publish online</p>
+		</div>
+		<div class="col col3">
+			<div class="image"><img src="/images/browser.png" alt="Key Phrases" /></div>
+			<h3>Key Phrases</h3>
+			<p>Search and customize thousands of professional phrases. You pick from samples written by experts. No more writers block.</p>
+			<p>We include context-sensitive resume writing tips</p>
+		</div>
+	</div>
+	<div class="secondary two-col">
+		<div class="col col1">
+			<h3>Heading</h3>
+			<ul class="arrow">
+				<li>List item</li>
+				<li>List item</li>
+				<li>List item</li>
+				<li>List item</li>
+			</ul>
+		</div>
+		<div class="col col2">
+			<h3>Heading</h3>
+			<ul class="arrow">
+				<li>List item</li>
+				<li>List item</li>
+				<li>List item</li>
+				<li>List item</li>
+			</ul>
+		</div>
+	</div>
+</main>
