66fff9800d99263e422181bc63baa4725d964730
zrichard <z.c.richard11@gmail.com>
2014-03-25 14:10:22 -0400

    css changes

--- rsm_application/views/examples.php
+++ rsm_application/views/examples.php
@@ -6,11 +6,36 @@
 <div class="three-col" style="text-align:center">
 	<div class="col col1">
 		<a href=""><img class="resume-thumb" src="http://placehold.it/250x325"></a>
+		<p class="rez-thumb-cap"><strong>Marketing Specialist</strong><br /><em>Executive Design</em></p>
 	</div>
 	<div class="col col2">
 		<a href=""><img class="resume-thumb" src="http://placehold.it/250x325"></a>
+		<p class="rez-thumb-cap"><strong>Marketing Specialist</strong><br /><em>Executive Design</em></p>
 	</div>
 	<div class="col col3">
 		<a href=""><img class="resume-thumb" src="http://placehold.it/250x325"></a>
+		<p class="rez-thumb-cap"><strong>Marketing Specialist</strong><br /><em>Executive Design</em></p>
+	</div>
+</div>
+<div class="secondary">
+	<div class="col col1">
+		<h3>Most Popular Resume Examples</h3>
+		<ul>
+			<li>Business Analyst</li>
+			<li>Teacher</li>
+			<li>Engineer</li>
+			<li>Software Developer</li>
+			<li>Web Developer</li>
+		</ul>
+	</div>
+	<div class="col col2">
+		<h3>Most Popular Resume Templates</h3>
+		<ul>
+			<li>Executive Design</li>
+			<li>Contemporary Design</li>
+			<li>Modern Design</li>
+			<li>Creative Design</li>
+			<li>Tech Design</li>
+		</ul>
 	</div>
 </div>
