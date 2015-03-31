cc95a9786da40212dac096abc0f959b65aefd888
Nick Johnson <nicholas.johnson78@gmail.com>
2015-03-31 11:45:11 -0400

    turned off error reporting

--- public/index.php
+++ public/index.php
@@ -55,8 +55,7 @@ if (defined('ENVIRONMENT'))
 	
 		case 'testing':
 		case 'production':
-			//error_reporting(0);
-            error_reporting(E_ALL);
+			error_reporting(0);
 		break;
 
 		default:
